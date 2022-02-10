eea5935c4af61bda02ce1b4876a5d005fab1605c
Sean Hwang <hwang103@purdue.edu>
2022-02-09 19:01:47 -0600

    Update main.py

--- audioModel/main.py
+++ audioModel/main.py
@@ -16,3 +16,88 @@ from scipy.io import wavfile
 
 print(f"TensorFlow Version: {tf.__version__}")
 print(f"Model Maker Version: {mm.__version__}")
+
+birds_dataset_folder = tf.keras.utils.get_file('birds_dataset.zip',
+                                                'https://storage.googleapis.com/laurencemoroney-blog.appspot.com/birds_dataset.zip',
+                                                cache_dir='./',
+                                                cache_subdir='dataset',
+                                                extract=True)
+# @title [Run this] Util functions and data structures.
+
+data_dir = './dataset/small_birds_dataset'
+
+bird_code_to_name = {
+  'wbwwre1': 'White-breasted Wood-Wren',
+  'houspa': 'House Sparrow',
+  'redcro': 'Red Crossbill',  
+  'chcant2': 'Chestnut-crowned Antpitta',
+  'azaspi1': "Azara's Spinetail",   
+}
+
+birds_images = {
+  'wbwwre1': 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/22/Henicorhina_leucosticta_%28Cucarachero_pechiblanco%29_-_Juvenil_%2814037225664%29.jpg/640px-Henicorhina_leucosticta_%28Cucarachero_pechiblanco%29_-_Juvenil_%2814037225664%29.jpg', #   Alejandro Bayer Tamayo from Armenia, Colombia 
+  'houspa': 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/52/House_Sparrow%2C_England_-_May_09.jpg/571px-House_Sparrow%2C_England_-_May_09.jpg', #    Diliff
+  'redcro': 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/49/Red_Crossbills_%28Male%29.jpg/640px-Red_Crossbills_%28Male%29.jpg', #  Elaine R. Wilson, www.naturespicsonline.com
+  'chcant2': 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/67/Chestnut-crowned_antpitta_%2846933264335%29.jpg/640px-Chestnut-crowned_antpitta_%2846933264335%29.jpg', #   Mike's Birds from Riverside, CA, US
+  'azaspi1': 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Synallaxis_azarae_76608368.jpg/640px-Synallaxis_azarae_76608368.jpg', # https://www.inaturalist.org/photos/76608368
+}
+
+test_files = os.path.abspath(os.path.join(data_dir, 'test/*/*.wav'))
+
+def get_random_audio_file():
+  test_list = glob.glob(test_files)
+  random_audio_path = random.choice(test_list)
+  return random_audio_path
+
+
+def show_bird_data(audio_path):
+  sample_rate, audio_data = wavfile.read(audio_path, 'rb')
+
+  bird_code = audio_path.split('/')[-2]
+  print(f'Bird name: {bird_code_to_name[bird_code]}')
+  print(f'Bird code: {bird_code}')
+  display(Image(birds_images[bird_code]))
+
+  plttitle = f'{bird_code_to_name[bird_code]} ({bird_code})'
+  plt.title(plttitle)
+  plt.plot(audio_data)
+  display(Audio(audio_data, rate=sample_rate))
+
+print('functions and data structures created')
+
+random_audio = get_random_audio_file()
+show_bird_data(random_audio) 
+
+train_data = audio_classifier.DataLoader.from_folder(
+    spec, os.path.join(data_dir, 'train'), cache=True)
+
+train_data, validation_data = train_data.split(0.8)
+
+test_data = audio_classifier.DataLoader.from_folder(
+    spec, os.path.join(data_dir, 'test'), cache=True) 
+
+batch_size = 128
+epochs = 100
+
+print('Training the model')
+model = audio_classifier.create(
+    train_data,
+    spec,
+    validation_data,
+    batch_size=batch_size,
+    epochs=epochs)
+
+
+def show_confusion_matrix(confusion, test_labels):
+  """Compute confusion matrix and normalize."""
+  confusion_normalized = confusion.astype("float") / confusion.sum(axis=1)
+  axis_labels = test_labels
+  ax = sns.heatmap(
+      confusion_normalized, xticklabels=axis_labels, yticklabels=axis_labels,
+      cmap='Blues', annot=True, fmt='.2f', square=True)
+  plt.title("Confusion matrix")
+  plt.ylabel("True label")
+  plt.xlabel("Predicted label")
+
+confusion_matrix = model.confusion_matrix(test_data)
+show_confusion_matrix(confusion_matrix.numpy(), test_data.index_to_label) 
