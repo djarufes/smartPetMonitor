7cb221f53102480fbb78bfe53cfbe222c544b906
Sean Hwang <hwang103@purdue.edu>
2022-02-26 23:44:24 -0500

    Update main.py

--- audioModel/main.py
+++ audioModel/main.py
@@ -1,103 +1,154 @@
-import tensorflow as tf
-import tflite_model_maker as mm
-from tflite_model_maker import audio_classifier
-import os
-
+import librosa
+import pandas as pd
 import numpy as np
+from tqdm import tqdm
+from scipy.io import wavfile as wav
 import matplotlib.pyplot as plt
-import seaborn as sns
+import matplotlib.pyplot as plt
 
-import itertools
-import glob
-import random
+import tensorflow as tf 
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense,Dropout,Activation,Flatten
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.callbacks import ModelCheckpoint
+from tensorflow.keras.utils import to_categorical
+from sklearn.preprocessing import LabelEncoder
+from sklearn.model_selection import train_test_split
+from sklearn import metrics  
 
-from IPython.display import Audio, Image
-from scipy.io import wavfile
+from datetime import datetime 
+import random 
+import os 
 
-print(f"TensorFlow Version: {tf.__version__}")
-print(f"Model Maker Version: {mm.__version__}")
+print(tf.__version__)
 
-birds_dataset_folder = tf.keras.utils.get_file('birds_dataset.zip',
-                                                'https://storage.googleapis.com/laurencemoroney-blog.appspot.com/birds_dataset.zip',
-                                                cache_dir='./',
-                                                cache_subdir='dataset',
-                                                extract=True)
-# @title [Run this] Util functions and data structures.
+audio_file_path='datasets/drinking_2.wav'
+librosa_audio_data,librosa_sample_rate=librosa.load(audio_file_path)
+print(librosa_audio_data)
 
-data_dir = './dataset/small_birds_dataset'
+plt.figure(figsize=(12, 4))
+plt.plot(librosa_audio_data) 
 
-bird_code_to_name = {
-  'wbwwre1': 'White-breasted Wood-Wren',
-  'houspa': 'House Sparrow',
-  'redcro': 'Red Crossbill',  
-  'chcant2': 'Chestnut-crowned Antpitta',
-  'azaspi1': "Azara's Spinetail",   
-}
-
-birds_images = {
-  'wbwwre1': 'https://upload.wikimedia.org/wikipedia/commons/thumb/2/22/Henicorhina_leucosticta_%28Cucarachero_pechiblanco%29_-_Juvenil_%2814037225664%29.jpg/640px-Henicorhina_leucosticta_%28Cucarachero_pechiblanco%29_-_Juvenil_%2814037225664%29.jpg', #   Alejandro Bayer Tamayo from Armenia, Colombia 
-  'houspa': 'https://upload.wikimedia.org/wikipedia/commons/thumb/5/52/House_Sparrow%2C_England_-_May_09.jpg/571px-House_Sparrow%2C_England_-_May_09.jpg', #    Diliff
-  'redcro': 'https://upload.wikimedia.org/wikipedia/commons/thumb/4/49/Red_Crossbills_%28Male%29.jpg/640px-Red_Crossbills_%28Male%29.jpg', #  Elaine R. Wilson, www.naturespicsonline.com
-  'chcant2': 'https://upload.wikimedia.org/wikipedia/commons/thumb/6/67/Chestnut-crowned_antpitta_%2846933264335%29.jpg/640px-Chestnut-crowned_antpitta_%2846933264335%29.jpg', #   Mike's Birds from Riverside, CA, US
-  'azaspi1': 'https://upload.wikimedia.org/wikipedia/commons/thumb/b/b2/Synallaxis_azarae_76608368.jpg/640px-Synallaxis_azarae_76608368.jpg', # https://www.inaturalist.org/photos/76608368
-}
-
-test_files = os.path.abspath(os.path.join(data_dir, 'test/*/*.wav'))
-
-def get_random_audio_file():
-  test_list = glob.glob(test_files)
-  random_audio_path = random.choice(test_list)
-  return random_audio_path
+wave_sample_rate, wave_audio = wav.read(audio_file_path)  
 
 
-def show_bird_data(audio_path):
-  sample_rate, audio_data = wavfile.read(audio_path, 'rb')
+# Original audio in stereo
+plt.figure(figsize=(12, 4))
+plt.plot(wave_audio)
 
-  bird_code = audio_path.split('/')[-2]
-  print(f'Bird name: {bird_code_to_name[bird_code]}')
-  print(f'Bird code: {bird_code}')
-  display(Image(birds_images[bird_code]))
+# MFCC Extraction begin here 
+mfccs = librosa.feature.mfcc(y=librosa_audio_data, sr=librosa_sample_rate, n_mfcc=40)
+print(mfccs.shape) 
 
-  plttitle = f'{bird_code_to_name[bird_code]} ({bird_code})'
-  plt.title(plttitle)
-  plt.plot(audio_data)
-  display(Audio(audio_data, rate=sample_rate))
+audio_dataset_path='datasets'
+metadata=pd.read_csv('metadata.csv')
+metadata.head(50)
 
-print('functions and data structures created')
+for index_num,row in tqdm(metadata.iterrows()):
+    file_name = os.path.join(os.path.abspath(audio_dataset_path),str(row["file_name"]))
+    print(file_name)
 
-random_audio = get_random_audio_file()
-show_bird_data(random_audio) 
+# Loop for extracting
+extracted_features=[]
+for index_num,row in tqdm(metadata.iterrows()):
+    file_name = os.path.join(os.path.abspath(audio_dataset_path),str(row["file_name"])+'.wav')
+    print(file_name)
+    final_class_labels=row["class"]
+    data=features_extractor(file_name)
+    extracted_features.append([data,final_class_labels]) 
 
-train_data = audio_classifier.DataLoader.from_folder(
-    spec, os.path.join(data_dir, 'train'), cache=True)
+# converting extracted spectral features of drinking to Pandas dataframe
+extracted_features_df=pd.DataFrame(extracted_features,columns=['feature','class'])
+extracted_features_df.head(50) 
 
-train_data, validation_data = train_data.split(0.8)
-
-test_data = audio_classifier.DataLoader.from_folder(
-    spec, os.path.join(data_dir, 'test'), cache=True) 
-
-batch_size = 128
-epochs = 100
-
-print('Training the model')
-model = audio_classifier.create(
-    train_data,
-    spec,
-    validation_data,
-    batch_size=batch_size,
-    epochs=epochs)
+# Split the dataset into independent and dependent dataset
+X=np.array(extracted_features_df['feature'].tolist())
+y=np.array(extracted_features_df['class'].tolist())
+X.shape 
 
 
-def show_confusion_matrix(confusion, test_labels):
-  """Compute confusion matrix and normalize."""
-  confusion_normalized = confusion.astype("float") / confusion.sum(axis=1)
-  axis_labels = test_labels
-  ax = sns.heatmap(
-      confusion_normalized, xticklabels=axis_labels, yticklabels=axis_labels,
-      cmap='Blues', annot=True, fmt='.2f', square=True)
-  plt.title("Confusion matrix")
-  plt.ylabel("True label")
-  plt.xlabel("Predicted label")
+### MODEL TRAINING from here onwards ###
+labelencoder=LabelEncoder()
+y=to_categorical(labelencoder.fit_transform(y))
 
-confusion_matrix = model.confusion_matrix(test_data)
-show_confusion_matrix(confusion_matrix.numpy(), test_data.index_to_label) 
+### Train Test Split
+X_train,X_test,y_train,y_test=train_test_split(X,y,test_size=0.2,random_state=0) 
+
+### No of classes
+num_labels=y.shape[1] 
+
+model=Sequential()
+###first layer
+model.add(Dense(100,input_shape=(40,)))
+model.add(Activation('relu'))
+model.add(Dropout(0.5))
+###second layer
+model.add(Dense(200))
+model.add(Activation('relu'))
+model.add(Dropout(0.5))
+###third layer
+model.add(Dense(100))
+model.add(Activation('relu'))
+model.add(Dropout(0.5))
+
+###final layer
+model.add(Dense(num_labels))
+model.add(Activation('softmax'))
+model.compile(loss='categorical_crossentropy',metrics=['accuracy'],optimizer='adam') 
+
+# Trianing... 
+num_epochs = 100
+num_batch_size = 32
+
+checkpointer = ModelCheckpoint(filepath='models/drinking_rec.hdf5', 
+                               verbose=1, save_best_only=True)
+start = datetime.now()
+model.fit(X_train, y_train, batch_size=num_batch_size, epochs=num_epochs, validation_data=(X_test, y_test), callbacks=[checkpointer], verbose=1)
+
+duration = datetime.now() - start
+print("Training completed in time: ", duration)
+
+test_accuracy=model.evaluate(X_test,y_test,verbose=0)
+print("Test Accuracy is: ", test_accuracy[1]) 
+
+#Randomly select a file from validation set 
+drink_or_eat = str(random.randint(0,1))
+file_num = str(random.randint(1,22))
+if drink_or_eat == '0': 
+    string="drinking"
+elif drink_or_eat == '1': 
+    string ="eating"
+testfile = string+"_"+file_num+".wav"
+
+#File to be analyzed
+filename='datasets/'+testfile
+print("File to be analyzed by the model: "+filename,"\n")
+
+#Feeding the audio data into the model
+audio, sample_rate = librosa.load(filename, res_type='kaiser_fast') 
+mfccs_features = librosa.feature.mfcc(y=audio, sr=sample_rate, n_mfcc=40)
+mfccs_scaled_features = np.mean(mfccs_features.T,axis=0)
+librosa_audio_data,librosa_sample_rate=librosa.load(filename)
+plt.figure(figsize=(12, 4))
+plt.plot(librosa_audio_data)
+
+print("Extracted spectral feature array: \n")
+print(mfccs_scaled_features)
+
+mfccs_scaled_features=mfccs_scaled_features.reshape(1,-1)
+predicted_label=(model.predict(mfccs_scaled_features) > 0.5).astype("int32")
+print("\nModel outcome Label: ", predicted_label)
+
+#Interpreting the analyzed data from model 
+if predicted_label[0][0] == 1: 
+    final_behavior = "Drinking"
+elif predicted_label[0][0] == 0: 
+    final_behavior = "Eating"
+print("\nFinal classified behavior: "+final_behavior)
+
+def features_extractor(file):
+    audio, sample_rate = librosa.load(file_name, res_type='kaiser_fast') 
+    mfccs_features = librosa.feature.mfcc(y=audio, sr=sample_rate, n_mfcc=40)
+    mfccs_scaled_features = np.mean(mfccs_features.T,axis=0)
+    
+    return mfccs_scaled_features 
