c60ba12141d34936373771e26037e7147c0abb6a
Sean Hwang <hwang103@purdue.edu>
2022-04-14 17:33:15 -0400

    Update data_to_graph.py

--- systemController/data_to_graph.py
+++ systemController/data_to_graph.py
@@ -1,3 +1,32 @@
 ''' 
 System Controller - Data visualization
 '''
+
+import pandas as pd
+import matplotlib.pyplot as plt
+
+audio_data = pd.read_csv('audio_data.csv')
+audio_data = audio_data.dropna(how='all')
+audio_data = audio_data.dropna(axis='columns',how='all')
+# print(audio_data)
+
+time = audio_data['timestamp'].to_list()
+eating = audio_data['eat'].to_list()
+drinking = audio_data['drink'].to_list()
+# print(time,eating,drinking)
+
+# Eating Behavior Graph
+# Time vs Time - On Off graph
+plt.plot(time,eating)
+plt.xlabel("Timestamp")
+plt.ylabel("Time ")
+plt.title("Time spent eating over time")
+plt.xticks(ticks=time)
+plt.locator_params(axis='x', nbins = len(time)/2)
+plt.show()
+
+# Drinking Behavior Graph
+# Amount vs time
+# Group 5 data, add total drinking seconds.
+# Rate => 0.4mL water consumed per second
+
